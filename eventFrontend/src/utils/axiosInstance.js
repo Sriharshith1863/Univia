27f4add32a5f69813ddb858e9e13671b60f78bbe
sriharshith-2301cs56 <sriharshith_2301cs56@iitp.ac.in>
2025-11-27 17:06:46 +0530

    modify render deployed link

--- eventFrontend/src/utils/axiosInstance.js
+++ eventFrontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://univia.onrender.com/",
+    baseURL: "https://univia.onrender.com/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
